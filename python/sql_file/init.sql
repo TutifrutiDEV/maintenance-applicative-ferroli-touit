cd8654f4f106fbcb29debad21b0b85359cfda690
Tutifruti <64048607+TutifrutiDEV@users.noreply.github.com>
2024-02-02 17:02:40 +0100

    Ajout table critique

--- python/sql_file/init.sql
+++ python/sql_file/init.sql
@@ -17,3 +17,15 @@ CREATE TABLE users (
     name varchar(255) not null,
     password varchar(255) not null
 );
+
+CREATE TABLE `critiques` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `attraction_id` int NOT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `prenom` varchar(255) DEFAULT NULL,
+  `texte` text NOT NULL,
+  `note` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `attraction_id` (`attraction_id`),
+  CONSTRAINT `critiques_ibfk_1` FOREIGN KEY (`attraction_id`) REFERENCES `attraction` (`attraction_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
